2d7ea4c05d3ac87ec96b07e7969fcb942164788c
Noah van der Aa <ndvdaa@gmail.com>
2021-12-27 23:54:19 +0100

    Initial commit

---
+++ src/main/java/me/noahvdaa/makethemthink/makethemthink/MakeThemThink.java
@@ -0,0 +1,25 @@
+package me.noahvdaa.makethemthink.makethemthink;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.EntitiesLoadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MakeThemThink extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onEntitiesLoad(EntitiesLoadEvent event) {
+		event.getEntities().forEach(entity -> {
+			if (entity instanceof LivingEntity livingEntity) {
+				livingEntity.setAI(true);
+			}
+		});
+	}
+
+}
